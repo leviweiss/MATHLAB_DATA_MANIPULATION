d32bd5e82548e331290e56aeba6a98d012d31a3f
Levi Weiss <levi.weiss3@gmail.com>
2019-09-04 17:12:32 +0300

    had bug in the way looking at the data. changed all the implemention and also seperated MZ from intensity and also finished the function with constant size

--- bio.m
+++ bio.m
@@ -1,35 +1,48 @@
 % constants
 global PPM;
 PPM = 5;
+global numberOfSamples;
+numberOfSamples = 6;
+global selectedIndexOfScan;
+selectedIndexOfScan = 1;
+global data;
 
-% basic variables
-selectedDataSample = data{1, 1};
-numberOfScans = size(selectedDataSample.scan, 1);
+% preperation
+data = load('matlab.mat');
 
-% creating the sorted frame of data
-maxScanSize = getMaxScanSize(numberOfScans, selectedDataSample.scan);
-allPeaks = getMatrixWithAllPeaks(selectedDataSample, numberOfScans, maxScanSize);
-allPeaksSorted = sort(allPeaks);
+% creating the matrix of data
+maxScanSize = getMaxScanSize();
+allPeaks = getMatrixWithAllPeaks(maxScanSize);
+[allPeaksMZ, allPeaksIntensity] = seperateIntensityFromMZ(allPeaks);
 
 % creating the matched data
-% matchedWithoutAnyProcessing = withoutAnyProcessing(allPeaksSorted, maxScanSize, numberOfScans, 1);
-matchedWithConstantBuckets = processingWithConstantBuckets(allPeaksSorted, maxScanSize, numberOfScans);
+% matchedWithoutAnyProcessing = withoutAnyProcessing(allPeaksMZ, maxScanSize / 2, 1);
+matchedWithConstantBuckets = processingWithConstantBuckets(allPeaksMZ, maxScanSize / 2);
 
 
 
 
 % functions
-function matchedMatrix = processingWithConstantBuckets(allPeaksSorted, maxScanSize, numberOfScans)
+function [allPeaksMZ, allPeaksIntensity] = seperateIntensityFromMZ(allPeaks)
+allPeaksMZ = allPeaks(1:2:end,:);
+allPeaksIntensity = allPeaks(2:2:end,:);
+end
 
-matchedMatrix = NaN(maxScanSize, numberOfScans);
+
+
+
+function matchedMatrix = processingWithConstantBuckets(allPeaks, maxScanSize)
+
+global numberOfSamples;
+matchedMatrix = NaN(maxScanSize, numberOfSamples);
 bucketSize = 100;
 numberOfBuckets = maxScanSize / bucketSize;
 startingRow = 1;
 endingRow = bucketSize;
 startingCounter = 1;
 for currBucketNumber = 1:numberOfBuckets
-    allPeaksSortedCutted = allPeaksSorted(startingRow:endingRow, :);
-    [bucketMatchedMatrix, startingCounter] = withoutAnyProcessing(allPeaksSortedCutted, bucketSize, numberOfScans, startingCounter);
+    allPeaksCutted = allPeaks(startingRow:endingRow, :);
+    [bucketMatchedMatrix, startingCounter] = withoutAnyProcessing(allPeaksCutted, bucketSize, startingCounter);
     matchedMatrix(startingRow:endingRow, :) = bucketMatchedMatrix;
     startingRow = startingRow + bucketSize;
     endingRow = endingRow + bucketSize;
@@ -37,28 +50,33 @@ end
 
 reminder = rem(maxScanSize, bucketSize);
 if reminder ~= 0
-    
+    endingRow = startingRow + reminder - 1;
+    bucketSize = reminder;
+    allPeaksCutted = allPeaks(startingRow:endingRow, :);
+    bucketMatchedMatrix = withoutAnyProcessing(allPeaksCutted, bucketSize, startingCounter);
+    matchedMatrix(startingRow:endingRow, :) = bucketMatchedMatrix;
 end
 
 end
 
 
-function [matchedMatrix, counter] = withoutAnyProcessing(allPeaksSorted, maxScanSize, numberOfScans, startingCounter)
+function [matchedMatrix, counter] = withoutAnyProcessing(allPeaks, maxScanSize, startingCounter)
 
 global PPM;
+global numberOfSamples;
 counter = startingCounter;
-matchedMatrix = NaN(maxScanSize, numberOfScans);
-    for column = 1:numberOfScans
+matchedMatrix = NaN(maxScanSize, numberOfSamples);
+    for column = 1:numberOfSamples
         for row = 1:maxScanSize
-            number = allPeaksSorted(row, column);
+            number = allPeaks(row, column);
             counterInMatchedMatrix = matchedMatrix(row, column);
             if isnan(counterInMatchedMatrix) && ~isnan(number)
                 matchedMatrix(row, column) = counter;
                 lowerThreshold = number - number * PPM / 10 .^ 6;
                 upperThreshold = number + number * PPM / 10 .^ 6;
-                for columnInternal = (column+1):numberOfScans
+                for columnInternal = (column+1):numberOfSamples
                     for rowInternal = 1:maxScanSize
-                        numberInternal = allPeaksSorted(rowInternal, columnInternal);
+                        numberInternal = allPeaks(rowInternal, columnInternal);
                         if ~isnan(numberInternal)
                             if (numberInternal >= lowerThreshold) && (numberInternal <= upperThreshold)
                                 matchedMatrix(rowInternal, columnInternal) = counter;
@@ -75,25 +93,30 @@ end
 
 
 
-function allPeaks = getMatrixWithAllPeaks(selectedDataSample, numberOfScans, maxScanSize)
+function allPeaks = getMatrixWithAllPeaks(maxScanSize)
 
-allPeaks = NaN(maxScanSize, numberOfScans);
-for currScan = 1:numberOfScans
-    currPeaks = selectedDataSample.scan(currScan).peaks.mz;
+global numberOfSamples;
+global data;
+global selectedIndexOfScan;
+allPeaks = NaN(maxScanSize, numberOfSamples);
+for currSample = 1:numberOfSamples
+    currPeaks = data.data{1, currSample}.scan(selectedIndexOfScan).peaks.mz;
     delta = maxScanSize - size(currPeaks, 1);
     currPeaksWithCorrectSize = [currPeaks; NaN(delta, 1)];
-    allPeaks(:, currScan) = currPeaksWithCorrectSize;
+    allPeaks(:, currSample) = currPeaksWithCorrectSize;
 end
     
 end
 
 
-function maxSize = getMaxScanSize(numberOfScans, scans)
+function maxSize = getMaxScanSize()
 
+global numberOfSamples;
+global data;
 maxSize = 0;
-for currScan = 1:numberOfScans
-    currPeaks = scans(currScan).peaks.mz;
-    currSize = size(currPeaks, 1);
+for currSample = 1:numberOfSamples
+    currSize = data.data{1, currSample}.scan.peaksCount;
+    currSize = currSize * 2;
     if currSize > maxSize
         maxSize = currSize;
     end
